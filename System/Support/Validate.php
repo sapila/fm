745266dfe0a198c46df29c08b26cd4d524efaa91
sapila <nik.gkogk@hotmail.com>
2016-05-18 00:33:10 +0300

    small example added

--- System/Support/Validate.php
+++ System/Support/Validate.php
@@ -10,3 +10,42 @@ class ClassName
 	
 }
 
+ <?php
+
+$_POST = array('username' => 'nikos', 
+               'password' => 'pass');
+
+class  Validator {
+    
+    public static function check(array $data, array $rules){
+        
+        foreach ($rules as $key => $value) {
+            //parse rules for every item
+            $itemRules = explode("|",$value);
+            
+            foreach ($itemRules as $rule) {
+                //execute for every parsed rule
+                    switch ($rule) {
+                        case 'required':
+                             echo 'check if ' . $data[$key] . ' is required<br>';
+                            break;
+                        case 'isemail':
+                             echo 'check if ' . $data[$key] . ' is mail <br>';;
+                            break;
+                        default:
+                            echo 'error';
+                    }
+            }            
+        }
+        
+        return "done";
+    }
+}
+
+
+echo Validator::check($_POST,array(
+    'username' => 'required|isemail',
+    'password' => 'required|isemail'
+)) ;
+
+?>
